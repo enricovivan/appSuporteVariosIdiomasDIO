9987b422c89e627f838dc8f2c42d43936c07f50b
Enrico Vivan <enricovivan21@gmail.com>
2024-01-23 14:28:34 -0300

    uso de strings para 3 línguas diferentes

--- app/src/main/java/br/com/enricovivan/appcomsuporteavariosidiomas/MainActivity.kt
+++ app/src/main/java/br/com/enricovivan/appcomsuporteavariosidiomas/MainActivity.kt
@@ -3,12 +3,20 @@ package br.com.enricovivan.appcomsuporteavariosidiomas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.enricovivan.appcomsuporteavariosidiomas.ui.theme.AppComSuporteAVariosIdiomasTheme
 
@@ -17,30 +25,47 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AppComSuporteAVariosIdiomasTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+
+                // O desafio se consiste em criar um app internacionalizado
+                // onde o app se adequa à localização do dispositivo
+
+                // Primeiro, é criada uma superfície que preenche toda a tela do dispositivo
+                TelaCheiaCentralizado {
+                    Text(text = stringResource(id = R.string.text_firstScreen), color = MaterialTheme.colorScheme.primary)
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = stringResource(id = R.string.button_text))
+                    }
                 }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TelaCheiaCentralizado (
+    content: @Composable () -> Unit
+): Unit {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.primaryContainer),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        content()
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TelaCheiaCentralizadoPreview () {
     AppComSuporteAVariosIdiomasTheme {
-        Greeting("Android")
+        TelaCheiaCentralizado (content = {
+            Text(text = "allalala", color = MaterialTheme.colorScheme.primary)
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Me Aperte")
+            }
+        })
     }
 }
